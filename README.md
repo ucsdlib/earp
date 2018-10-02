118c2933c448376ff5d694cfb4d4b71684f1312b
Matt Critchlow <mcritchlow@ucsd.edu>
2018-10-01 21:12:56 -0700

    ldap: add initial service file and creds
    TODO:
      - get final credentials in place
      - sort out employees query
      - add unit tests

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 An application supporting an Employee Recognition program workflow
 
 ## Local Development
+### Secrets
+This app is using Rails 5.2+ encrypted secrets, stored in
+`config/credentials.yml.enc`. In order to access them, you'll need to set the
+master key in `config/master.key`, stored in LastPass currently (final location
+TBD)
+
+### Docker
 1. Install docker and docker-compose
 1. Run docker-compose file `docker-compose up`
 1. In a separate tab, setup the db `docker-compose exec web bin/rails
---
+++ app/services/ldap.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'net/ldap'
+
+# Ldap provides a connection service to an LDAP server for queries
+# Examples in this application are:
+#   - Load all current library staff to populate a form <select> list
+#   - Authorize an authenticated user to an LDAP group (admins)
+class Ldap
+  # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+  def ldap_connection
+    @ldap_connection ||= Net::LDAP.new(
+      host: Rails.application.credentials.ldap[:host],
+      port: Rails.application.credentials.ldap[:port],
+      base: Rails.application.credentials.ldap[:base],
+      encryption: { method: :simple_tls },
+      auth: {
+        method: :simple,
+        username: Rails.application.credentials.ldap[:username],
+        password: Rails.application.credentials.ldap[:password]
+      }
+    )
+  end
+  # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
+
+  # Query all currently active employees
+  # @return [Array] employee information (Name, Email)
+  def employees
+    result = []
+    ldap_connection.search(
+      filter: Net::LDAP::Filter.eq('objectcategory', 'user'),
+      attributes: %w[user email] # TODO: ask Dan
+    ) do |employee|
+      result << employee
+    end
+  end
+end

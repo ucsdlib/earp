983c8c8767c1308bf5c0900a577acbffe7799862
Matt Critchlow <mcritchlow@ucsd.edu>
2018-10-03 14:37:26 -0700

    auth: remove unused methods

--- app/helpers/auth_helper.rb
+++ app/helpers/auth_helper.rb
@@ -2,7 +2,6 @@
 
 # AuthHelper
 module AuthHelper
-  # TODO: sort out what's needed here..
   def current_user
     @current_user ||= User.find_by(uid: session[:user_id]) if session[:user_id]
   end
@@ -13,15 +12,5 @@ module AuthHelper
 
   def logged_in?
     current_user.present?
-  end
-
-  def authorize
-    # TODO: can this be: logged_in? || access_denied
-    logged_in? ? true : access_denied
-  end
-
-  def access_denied
-    flash[:alert] = 'You must sign in to perform this action.'
-    redirect_to new_user_session_path(origin: request.original_url) and return false
   end
 end

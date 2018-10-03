1d9091bd30e26e26d2590c971c427e3cd213e388
Matt Critchlow <mcritchlow@ucsd.edu>
2018-10-03 09:17:51 -0700

    models: add User table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_02_200759) do
+ActiveRecord::Schema.define(version: 2018_10_03_160922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 2018_10_02_200759) do
     t.text "description", null: false
     t.boolean "anonymous", default: false
     t.string "recognizer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: ""
+    t.string "full_name", default: ""
+    t.string "uid", default: "", null: false
+    t.string "provider", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

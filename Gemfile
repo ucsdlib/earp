cd88c7a3da906f97547cd5398bd2bd55239717aa
Matt Critchlow <mcritchlow@ucsd.edu>
2018-10-16 10:58:13 -0700

    tests: add coveralls and simplecov

--- Gemfile
+++ Gemfile
@@ -48,10 +48,12 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'capybara'
+  gem 'coveralls'
   gem 'rspec-rails'
   gem 'rubocop'
   gem 'rubocop-rspec'
   gem 'selenium-webdriver'
+  gem "simplecov", require: false
 end
 
 group :development do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,16 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+
+require 'simplecov'
+require 'coveralls'
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+  [
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
+)
+SimpleCov.start "rails"
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in

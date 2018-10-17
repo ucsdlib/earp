4c79d2318c26f6cf938a5812cb58822926007505
Matt Critchlow <mcritchlow@ucsd.edu>
2018-10-17 08:53:29 -0700

    tests: filter channels and mailers from simplecov report for now

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,7 +14,10 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
     Coveralls::SimpleCov::Formatter
   ]
 )
-SimpleCov.start "rails"
+SimpleCov.start :rails do
+  add_filter "/channels/"
+  add_filter "/mailers/"
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in

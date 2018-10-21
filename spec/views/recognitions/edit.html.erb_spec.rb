36b5dc89abdb54401df50e394c7b087129049900
Matt Critchlow <mcritchlow@ucsd.edu>
2018-10-20 20:29:17 -0700

    tests: remove scaffold cruft
    Revisit these areas when needed and test what's actually necessary

--- spec/views/recognitions/edit.html.erb_spec.rb
+++ spec/views/recognitions/edit.html.erb_spec.rb
@@ -1,33 +1,5 @@
 require 'rails_helper'
 
+# Recognition #edit placeholder
 RSpec.describe "recognitions/edit", type: :view do
-  before(:each) do
-    # @recognition = assign(:recognition, Recognition.create!(
-    #   :recognizee => "MyString",
-    #   :library_value => "MyString",
-    #   :description => "MyText",
-    #   :anonymous => false,
-    #   :recognizer => "MyString"
-    # ))
-  end
-
-  it "renders the edit recognition form" do
-    pending "get capybara tests in place"
-    render
-
-    assert_select "form[action=?][method=?]", recognition_path(@recognition), "post" do
-
-      # TODO: find better way to test this
-      # assert_select "select[name=?]", "recognition[recognizee]"
-
-      assert_select "input[name=?]", "recognition[library_value]"
-
-      assert_select "textarea[name=?]", "recognition[description]"
-
-      assert_select "input[name=?]", "recognition[anonymous]"
-
-      # TODO: find better way to test this
-      # assert_select "input[name=?]", "recognition[recognizer]"
-    end
-  end
 end

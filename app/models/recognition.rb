d8cf46115e6fd17acbd8327af640dc022bca6c57
Matt Critchlow <mcritchlow@ucsd.edu>
2018-11-07 16:00:49 -0800

    recognition: add .created_between for statistics

--- app/models/recognition.rb
+++ app/models/recognition.rb
@@ -6,4 +6,9 @@ class Recognition < ApplicationRecord
   belongs_to :employee
 
   validates :library_value, :description, presence: true
+
+  # A custom finder used for identifying records created between a given date range
+  def self.created_between(start_date, end_date)
+    where(created_at: start_date..end_date)
+  end
 end

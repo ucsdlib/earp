f8a4f3387659fc4f1dca57c342eb2c44ba4c1da7
Matt Critchlow <mcritchlow@ucsd.edu>
2018-11-08 11:31:44 -0800

    auth-helper: get require_administrator working as developer

--- app/helpers/auth_helper.rb
+++ app/helpers/auth_helper.rb
@@ -14,10 +14,13 @@ module AuthHelper
     current_user.present?
   end
 
+  def valid_administrator?
+    User.administrator?(current_user.id) || current_user.developer?
+  end
+
   # Require that a current user is authenticated and an administrator
   def require_administrator
-    redirect_to recognitions_path unless logged_in?# && User.administrator?(current_user)
-    # TODO: add check for developer mode so this can work without SSO
-    # redirect_to recognitions_path unless logged_in? && User.administrator?(current_user)
+    # redirect_to recognitions_path unless logged_in?# && User.administrator?(current_user)
+    redirect_to recognitions_path unless logged_in? && valid_administrator?
   end
 end

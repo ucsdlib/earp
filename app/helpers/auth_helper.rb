4b44db0f9f0d03189d9f07b912d43bc285681e21
Matt Critchlow <mcritchlow@ucsd.edu>
2018-11-07 16:00:21 -0800

    [wip]auth-helper: add require_administrator check

--- app/helpers/auth_helper.rb
+++ app/helpers/auth_helper.rb
@@ -13,4 +13,11 @@ module AuthHelper
   def logged_in?
     current_user.present?
   end
+
+  # Require that a current user is authenticated and an administrator
+  def require_administrator
+    redirect_to recognitions_path unless logged_in?# && User.administrator?(current_user)
+    # TODO: add check for developer mode so this can work without SSO
+    # redirect_to recognitions_path unless logged_in? && User.administrator?(current_user)
+  end
 end

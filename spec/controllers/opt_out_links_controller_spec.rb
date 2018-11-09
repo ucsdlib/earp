2b7e77239b9586a24a20c9e861a46b0357c09ad8
Matt Critchlow <mcritchlow@ucsd.edu>
2018-11-09 15:19:02 -0800

    optoutlinks: add controller and routing

---
+++ spec/controllers/opt_out_links_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe OptOutLinksController, type: :controller do
+  describe "GET #optout" do
+    let!(:recognition) { FactoryBot.create(:recognition,
+                                            user: user,
+                                            created_at: Time.parse('2018-10-01'),
+                                            library_value: 'collab',
+                                            description: 'perhaps something not nice',
+                                            employee: employee) }
+    let(:employee) { FactoryBot.create(:employee) }
+    let(:user) { FactoryBot.create(:user) }
+
+    it 'should suppress the recognition with a valid key' do
+      get :optout, params: { key: OptOutLink.first.key }
+      expect(Recognition.find_by(id: recognition.id).suppressed).to be(true)
+    end
+
+    it 'should do nothing with an valid key' do
+      get :optout, params: { key: 'utter-nonsense' }
+      expect(recognition.suppressed).to be(false)
+    end
+  end
+end

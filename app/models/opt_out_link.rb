3b2ab2d3b05727b641f59a9b9e32209820fc1aef
Matt Critchlow <mcritchlow@ucsd.edu>
2018-11-09 14:11:25 -0800

    optoutlinks: add expired links method and tests

--- app/models/opt_out_link.rb
+++ app/models/opt_out_link.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
+# OptOutLinks for Recogntions
 class OptOutLink < ApplicationRecord
+  # Will find and remove any OptOutLinks that have expired
+  def self.audit_expired_links
+    where('DATE(expires) <= ?', Time.zone.today).delete_all
+  end
 end
--- spec/factories/opt_out_links.rb
+++ spec/factories/opt_out_links.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :opt_out_link do
     key { "MyString" }
-    recognition_id { "" }
+    recognition_id { "1" }
     expires { "2018-11-09 21:35:20" }
   end
 end

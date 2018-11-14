68dd89861e6af94528f0206d1fec91498a45610e
Matt Critchlow <mcritchlow@ucsd.edu>
2018-11-14 09:50:21 -0800

    tests: remove useless helper test. covered by system tests

--- spec/helpers/recognitions_helper_spec.rb
+++ spec/helpers/recognitions_helper_spec.rb
@@ -1,7 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe RecognitionsHelper, type: :helper do
-  describe '#library_values' do
-    specify { expect(helper.library_values).to eq(LIBRARY_VALUES) }
-  end
 end

0a8b015e342c7f0c2bef19ba70024cad805210b6
Matt Critchlow <mcritchlow@ucsd.edu>
2018-11-27 09:25:06 -0800

    recognition form: sort employees in select list

--- spec/system/recognition_spec.rb
+++ spec/system/recognition_spec.rb
@@ -18,6 +18,15 @@ RSpec.describe 'interacting with recognitions', type: :system do
     expect(page).to have_current_path(recognitions_path)
   end
 
+  it 'sorts the employees by display name in the select list' do
+    sign_in
+    FactoryBot.create(:employee, display_name: 'Lincoln, Abraham')
+    FactoryBot.create(:employee, display_name: 'Washington, George')
+    FactoryBot.create(:employee, display_name: 'Adams, John')
+    visit new_recognition_path
+    expect(page.find_field('recognition_employee_id').text).to eq "Select employee to recognize Adams, John Lincoln, Abraham Washington, George"
+  end
+
   it 'does not allow a user to create a recognition without a recognizee, value, and description' do
     sign_in
     mock_employee_query

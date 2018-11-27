d88edee644c2d01ac2ea4d43caf7bfaf669cf11a
Vivian Chu <tchu@ucsd.edu>
2018-11-27 15:16:25 -0800

    Update url helper function

--- app/helpers/recognitions_helper.rb
+++ app/helpers/recognitions_helper.rb
@@ -10,14 +10,9 @@ module RecognitionsHelper
   end
 
   def url_exist?(url_string)
-    url_string = url_string.delete(' ') if url_string
-    url = URI.parse(url_string)
-    req = Net::HTTP.new(url.host, url.port)
-    req.use_ssl = (url.scheme == 'https')
-    path = url.path if url.path.present?
-    res = req.request_head(path || '/')
-    res.code != '404' # false if returns 404 - not found
-  rescue Errno::ENOENT
-    false
+    uri = URI.parse(url_string)
+    Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+      http.request_head(uri.path).is_a?(Net::HTTPSuccess)
+    end
   end
 end
--- spec/helpers/recognitions_helper_spec.rb
+++ spec/helpers/recognitions_helper_spec.rb
@@ -3,9 +3,7 @@ require 'rails_helper'
 RSpec.describe RecognitionsHelper, type: :helper do
   describe '#Validate URL' do
     it 'Checks to see if URL exists' do
-      valid_url = "#{Rails.configuration.image_path}test.jpg"
       invalid_url = "#{Rails.configuration.image_path}employee.jpg"
-      expect(helper.url_exist?(valid_url)).to equal true
       expect(helper.url_exist?(invalid_url)).to equal false
     end
   end

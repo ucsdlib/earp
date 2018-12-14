58c19359aa473ab0f034299e5b22e455204db6b1
Matt Critchlow <mcritchlow@ucsd.edu>
2018-12-14 10:42:17 -0800

    routes: fix homepage routing misconfiguration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :recognitions
   resources :statistics
-  root 'recognitions#home'
+  root 'recognitions#front'
 
   # RSS feed
   get 'feed', to: 'recognitions#feed', as: :feed

6beb367e65f0757790164f90565831d18574154a
Vivian Chu <tchu@ucsd.edu>
2018-12-13 16:25:20 -0800

    Update default url for staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options = {host: 'library.ucsd.edu'}
+  config.action_mailer.default_url_options = {host: 'lib-hifive-staging.ucsd.edu'}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

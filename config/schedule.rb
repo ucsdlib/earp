a5b638875ed9b9c8ed05a456903865d1684e66ed
Matt Critchlow <mcritchlow@ucsd.edu>
2019-01-22 14:55:29 -0800

    whenever: fix incorrect syntax causing midnight entries

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,13 @@
 set :output, { error: "log/cron_error.log", standard: "log/cron_standard.log" }
+# potentially use this for prod?
+# set :output, lambda { "2>&1 | logger -t high_five_cron" }
 #
 # Update the Employee table
-every 1.day at: '3:00 am' do
+every 1.day, at: '3:00 am' do
   rake "nightly:employees"
 end
 
 # Remove expired OptOutLinks
-every 1.day at: '2:00 am' do
+every 1.day, at: '2:00 am' do
   rake "nightly:expire_links"
 end

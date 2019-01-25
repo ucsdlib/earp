cf9c069c18f838c86c9e61843869abc1724bec40
Matt Critchlow <mcritchlow@ucsd.edu>
2019-01-25 09:50:05 -0800

    whenever: write to standard out/error

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,3 @@
-set :output, { error: "log/cron_error.log", standard: "log/cron_standard.log" }
-# potentially use this for prod?
-# set :output, lambda { "2>&1 | logger -t high_five_cron" }
-#
 # Update the Employee table
 every 1.day, at: '3:00 am' do
   rake "nightly:employees"

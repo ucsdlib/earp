c80a1b7c674e724f7936eab85a6ca91647fac5c0
Matt Critchlow <mcritchlow@ucsd.edu>
2019-03-01 09:26:41 -0800

    recognitions: add sustainability value

--- spec/factories/recognition.rb
+++ spec/factories/recognition.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 FactoryBot.define do
   factory :recognition do
-    library_value { %w[diversity collab innovate flexible service].sample }
+    library_value { %w[diversity collab innovate flexible service sustainable].sample }
     sequence(:description) { |n| "#{n} is so AMAZING!" }
   end
 end

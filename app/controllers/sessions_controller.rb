a917a253f9a3ac56939061ac09cb8ef4cd578850
Vivian Chu <tchu@ucsd.edu>
2019-03-05 11:37:30 -0800

    Update sign out message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -41,7 +41,7 @@ class SessionsController < ApplicationController
 
     # rubocop:disable Metrics/LineLength
     redirect_to logout_url,
-                notice: 'You have been logged out of High Five!. To logout of all Single Sign-On applications, close your browser'
+                notice: 'You have been logged out of High Five! To log out of all Single Sign-On applications, close your browser.'
     # rubocop:enable Metrics/LineLength
   end
 

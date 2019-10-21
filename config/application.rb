0641d66b02e3e559f691e1f47f8a0c87714368ac
Matt Critchlow <mcritchlow@ucsd.edu>
2019-10-21 15:04:23 -0700

    Create initial helm chart
    - Adds a initial working helm chart for High Five!
    - Adds a dependency on the postgresql helm chart
    - Adds a custom email container/service for sending app email via smtp
    - Adds a custom Rancher questions.yaml file to ease deployment in
      Rancher

--- config/application.rb
+++ config/application.rb
@@ -42,6 +42,10 @@ module Hifive
     config.action_mailer.delivery_method = ENV.fetch('APPS_H5_DELIVERY_METHOD').to_sym
     config.action_mailer.default_url_options = { host: ENV.fetch('APPS_H5_EMAIL_HOST') }
 
+    if ENV['APPS_H5_EMAIL_SMTP_HOST'].present? && ENV['APPS_H5_EMAIL_SMTP_PORT'].present?
+      config.action_mailer.smtp_settings = { address: ENV.fetch('APPS_H5_EMAIL_SMTP_HOST'),
+                                             port: ENV.fetch('APPS_H5_EMAIL_SMTP_PORT') }
+    end
     # Only production (staging eventually)
     config.shibboleth = false
   end

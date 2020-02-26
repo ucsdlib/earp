eb8c0c97dc4ca4bceeae8c707bfaf7abaf58e57b
Vivian Chu <tchu@ucsd.edu>
2020-02-26 12:37:28 -0800

    Update ldap query to use different security group
    Fix rubocop offenses
    
    Fix rubocop
    
    Fix rubocop

--- spec/services/ldap/filters_spec.rb
+++ spec/services/ldap/filters_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe Ldap::Filters, type: :service do
   describe '.employees' do
     it 'returns an LDAP filter for all Library employees' do
-      expect(described_class.employees.to_s).to eq('(&(&(&(EmployeeID=*)(ObjectCategory=person))(ObjectClass=user))(!(sAMAccountName=lib-*)))')
+      expect(described_class.employees.to_s).to eq('(&(objectCategory=user)(memberOf:1.2.840.113556.1.4.1941:=CN=All Library Staff,OU=Groups,OU=University Library,DC=AD,DC=UCSD,DC=EDU))')
     end
   end
 
   describe '.hifive_admin' do
     it 'returns an LDAP filter to check admin membership' do
-      expect(described_class.hifive_admin('drseuss').to_s).to eq('(&(&(CN=drseuss)(&(&(&(EmployeeID=*)(ObjectCategory=person))(ObjectClass=user))(!(sAMAccountName=lib-*))))(memberof=memberof=CN=lib-test))')
+      expect(described_class.hifive_admin('drseuss').to_s).to eq('(&(&(SAMAccountName=drseuss)(&(objectCategory=user)(memberOf:1.2.840.113556.1.4.1941:=CN=All Library Staff,OU=Groups,OU=University Library,DC=AD,DC=UCSD,DC=EDU)))(memberof=memberof=CN=lib-test))')
     end
   end
 end
